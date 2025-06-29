27a801b01b533fd13612d9e354143199634f767c
Zhang Zihao <orzaizyeee@sjtu.edu.cn>
2025-06-29 11:52:45 +0800

    build(Messenger): initialize level Level1 from sceneproto Game

--- src/Scenes/AllScenes.elm
+++ src/Scenes/AllScenes.elm
@@ -15,6 +15,7 @@ import Dict
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
 import Messenger.Scene.Scene exposing (AllScenes)
+import Scenes.Level1.Model as Level1
 
 
 {-| All Scenes
@@ -25,4 +26,5 @@ Store all the scenes with their name here.
 allScenes : AllScenes UserData SceneMsg
 allScenes =
     Dict.fromList
-        []
+        [ ( "Level1", Level1.scene )
+        ]
