8323048b19ec08bf4056a55188294bcdb345d1cd
Zhang Zihao <orzaizyeee@sjtu.edu.cn>
2025-06-29 11:45:41 +0800

    build(Messenger): initialize sceneproto Game

---
+++ src/SceneProtos/Game/Model.elm
@@ -0,0 +1,48 @@
+module SceneProtos.Game.Model exposing (genScene)
+
+{-| Scene configuration module
+
+@docs genScene
+
+-}
+
+import Lib.Base exposing (SceneMsg)
+import Lib.UserData exposing (UserData)
+import Messenger.Base exposing (Env, addCommonData)
+import Messenger.Scene.LayeredScene exposing (LayeredSceneEffectFunc, LayeredSceneLevelInit, LayeredSceneProtoInit, genLayeredScene, initCompose)
+import Messenger.Scene.Scene exposing (SceneStorage)
+import SceneProtos.Game.Init exposing (InitData)
+import SceneProtos.Game.SceneBase exposing (..)
+
+
+commonDataInit : Env () UserData -> Maybe InitData -> SceneCommonData
+commonDataInit _ _ =
+    {}
+
+
+init : LayeredSceneProtoInit SceneCommonData UserData LayerTarget LayerMsg SceneMsg InitData
+init env data =
+    let
+        cd =
+            commonDataInit env data
+
+        envcd =
+            addCommonData cd env
+    in
+    { renderSettings = []
+    , commonData = cd
+    , layers =
+        []
+    }
+
+
+settings : LayeredSceneEffectFunc SceneCommonData UserData LayerTarget LayerMsg SceneMsg
+settings _ _ _ =
+    []
+
+
+{-| Scene generator
+-}
+genScene : LayeredSceneLevelInit UserData SceneMsg InitData -> SceneStorage UserData SceneMsg
+genScene initd =
+    genLayeredScene (initCompose init initd) settings
