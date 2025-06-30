5cba91f4b4b2529552e6cea2a2ce50bda87ab8c6
Lin Mingqing <linmingqing@sjtu.edu.cn>
2025-06-30 20:59:30 +0800

    feat: render enemies

--- src/SceneProtos/Game/Components/Enemy/Model.elm
+++ src/SceneProtos/Game/Components/Enemy/Model.elm
@@ -6,21 +6,33 @@ module SceneProtos.Game.Components.Enemy.Model exposing (component)
 
 -}
 
+import Color
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
 import Messenger.Component.Component exposing (ComponentInit, ComponentMatcher, ComponentStorage, ComponentUpdate, ComponentUpdateRec, ComponentView, ConcreteUserComponent, genComponent)
 import REGL.BuiltinPrograms as P
-import SceneProtos.Game.Components.ComponentBase exposing (BaseData, ComponentMsg, ComponentTarget)
+import REGL.Common exposing (group)
+import REGL.Effects exposing (alphamult)
+import SceneProtos.Game.Components.ComponentBase exposing (BaseData, ComponentMsg(..), ComponentTarget)
+import SceneProtos.Game.Components.Enemy.Init exposing (..)
 import SceneProtos.Game.SceneBase exposing (SceneCommonData)
 
 
 type alias Data =
-    {}
+    { id : Int
+    , ty : String
+    , enemy : List Enemy
+    }
 
 
 init : ComponentInit SceneCommonData UserData ComponentMsg Data BaseData
 init env initMsg =
-    ( {}, () )
+    case initMsg of
+        EnemyInitMsg data ->
+            ( { enemy = data.enemy, id = data.id, ty = data.ty }, () )
+
+        _ ->
+            ( { enemy = [], id = 2, ty = "Enemy" }, () )
 
 
 update : ComponentUpdate SceneCommonData Data UserData SceneMsg ComponentTarget ComponentMsg BaseData
@@ -35,7 +47,28 @@ updaterec env msg data basedata =
 
 view : ComponentView SceneCommonData UserData Data BaseData
 view env data basedata =
-    ( P.empty, 0 )
+    ( group
+        [ alphamult 1 ]
+        (List.map
+            (\enemy ->
+                if enemy.enemytype == Dead then
+                    P.circle enemy.position 25 Color.blue
+
+                else
+                    let
+                        rectcolor =
+                            if enemy.enemytype == Normal then
+                                Color.green
+
+                            else
+                                Color.red
+                    in
+                    P.rect enemy.position ( 30, 60 ) rectcolor
+            )
+            data.enemy
+        )
+    , 2
+    )
 
 
 matcher : ComponentMatcher Data BaseData ComponentTarget
