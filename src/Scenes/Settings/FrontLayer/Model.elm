2052e8ce91b99d30bc4b67cc3e1daa99fa816229
Xiaotong Yin <in_child@sjtu.edu.cn>
2025-06-30 20:18:45 +0800

    build(Messenger): initialize layer FrontLayer under scene Settings

---
+++ src/Scenes/Settings/FrontLayer/Model.elm
@@ -0,0 +1,62 @@
+module Scenes.Settings.FrontLayer.Model exposing (layer)
+
+{-| Layer configuration module
+
+Set the Data Type, Init logic, Update logic, View logic and Matcher logic here.
+
+@docs layer
+
+-}
+
+import Lib.Base exposing (SceneMsg)
+import Lib.UserData exposing (UserData)
+import Messenger.GeneralModel exposing (Matcher)
+import Messenger.Layer.Layer exposing (ConcreteLayer, LayerInit, LayerStorage, LayerUpdate, LayerUpdateRec, LayerView, genLayer)
+import REGL.BuiltinPrograms as P
+import Scenes.Settings.SceneBase exposing (..)
+
+
+type alias Data =
+    {}
+
+
+init : LayerInit SceneCommonData UserData LayerMsg Data
+init env initMsg =
+    {}
+
+
+update : LayerUpdate SceneCommonData UserData LayerTarget LayerMsg SceneMsg Data
+update env evt data =
+    ( data, [], ( env, False ) )
+
+
+updaterec : LayerUpdateRec SceneCommonData UserData LayerTarget LayerMsg SceneMsg Data
+updaterec env msg data =
+    ( data, [], env )
+
+
+view : LayerView SceneCommonData UserData Data
+view env data =
+    P.empty
+
+
+matcher : Matcher Data LayerTarget
+matcher data tar =
+    tar == "FrontLayer"
+
+
+layercon : ConcreteLayer Data SceneCommonData UserData LayerTarget LayerMsg SceneMsg
+layercon =
+    { init = init
+    , update = update
+    , updaterec = updaterec
+    , view = view
+    , matcher = matcher
+    }
+
+
+{-| Layer generator
+-}
+layer : LayerStorage SceneCommonData UserData LayerTarget LayerMsg SceneMsg
+layer =
+    genLayer layercon
