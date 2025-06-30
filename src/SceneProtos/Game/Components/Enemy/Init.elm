ba4a35d264d57dc9e141bec671d1643af2ed0ab5
Lin Mingqing <linmingqing@sjtu.edu.cn>
2025-06-30 20:58:27 +0800

    feat: enemy type and corresponding records
    "re #9"

--- src/SceneProtos/Game/Components/Enemy/Init.elm
+++ src/SceneProtos/Game/Components/Enemy/Init.elm
@@ -1,4 +1,4 @@
-module SceneProtos.Game.Components.Enemy.Init exposing (InitData)
+module SceneProtos.Game.Components.Enemy.Init exposing (..)
 
 {-|
 
@@ -10,7 +10,33 @@ module SceneProtos.Game.Components.Enemy.Init exposing (InitData)
 -}
 
 
+type alias Bullet =
+    { position : ( Float, Float )
+    , attack : Float
+    }
+
+
+type Enemytype
+    = Normal
+    | Advanced
+    | Dead
+
+
+type alias Enemy =
+    { position : ( Float, Float )
+    , direction : Float -- 1 for right, -1 for left
+    , vx : Float
+    , hp : Float
+    , normal : Bool
+    , enemytype : Enemytype
+    }
+
+
 {-| The data used to initialize the scene
 -}
 type alias InitData =
-    {}
+    { id : Int
+    , ty : String
+    , enemy : List Enemy
+    , bullet : List Bullet
+    }
