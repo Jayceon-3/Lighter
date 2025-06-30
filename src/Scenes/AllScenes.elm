982ad69fd44e3e7b307457327f604af820c9b0c1
Xiaotong Yin <in_child@sjtu.edu.cn>
2025-06-30 20:02:56 +0800

    build(Messenger): initialize scene Home

--- src/Scenes/AllScenes.elm
+++ src/Scenes/AllScenes.elm
@@ -15,6 +15,7 @@ import Dict
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
 import Messenger.Scene.Scene exposing (AllScenes)
+import Scenes.Home.Model as Home
 import Scenes.Level1.Model as Level1
 
 
@@ -26,5 +27,6 @@ Store all the scenes with their name here.
 allScenes : AllScenes UserData SceneMsg
 allScenes =
     Dict.fromList
-        [ ( "Level1", Level1.scene )
+        [ ( "Home", Home.scene )
+        , ( "Level1", Level1.scene )
         ]
