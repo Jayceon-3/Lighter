a7a577a8d055a32e78d0f3f190fd783873077bf5
Lin Mingqing <linmingqing@sjtu.edu.cn>
2025-07-01 17:53:07 +0800

    refactor: fix rebase conlict

--- src/SceneProtos/Game/Components/ComponentBase.elm
+++ src/SceneProtos/Game/Components/ComponentBase.elm
@@ -10,11 +10,7 @@ module SceneProtos.Game.Components.ComponentBase exposing (..)
 -}
 
 import SceneProtos.Game.Components.Bullet.Init as BulletInit
-<<<<<<< HEAD
-import SceneProtos.Game.Components.Enemy.Init as EnemyInit
-=======
 import SceneProtos.Game.Components.Enemy.Init as EnemyInit exposing (..)
->>>>>>> caf196d (feat: side changes of decrease blood logic of enemies)
 import SceneProtos.Game.Components.Player.Init as PlayerInit
 import SceneProtos.Game.Components.Weapon.Init as WeaponInit
 
@@ -23,21 +19,15 @@ import SceneProtos.Game.Components.Weapon.Init as WeaponInit
 -}
 type ComponentMsg
     = NullComponentMsg
-<<<<<<< HEAD
+    | BulletInitMsg BulletInit.InitData
+    | EnemyInitMsg EnemyInit.InitData
     | PlayerInitMsg PlayerInit.InitData
     | WeaponInitMsg WeaponInit.InitData
-    | BulletInitMsg BulletInit.InitData
+    | Bullets (List BulletInit.SingleBullet)
+    | EnemyBullets (List EnemyInit.EnemyBullet)
     | PlayerStateMsg PlayerInit.State
     | WeaponDir Float
     | FireMsg BulletInit.SingleBullet
-=======
-    | BulletInitMsg BulletInit.InitData
->>>>>>> 0ee66e7 (feat(bullet and enemy): update the player bullet that can hurt the enemies)
-    | EnemyInitMsg EnemyInit.InitData
-    | PlayerInitMsg PlayerInit.InitData
-    | Bullets (List BulletInit.Bullet)
-    | EnemyBullets (List EnemyInit.EnemyBullet)
-    | PlayerMsg PlayerInit.State
 
 
 {-| Component target
